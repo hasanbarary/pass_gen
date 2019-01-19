2a14a6b1c7c1426a9b315281833245439f8d5ecd
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-19 07:02:03 +0300

    add username check

--- pass-gen.py
+++ pass-gen.py
@@ -8,10 +8,25 @@ def other():
     return chr(randint(33,42))
 def number():
     return str(randint(0,9))
+def request_username():
+    while True:
+        username=str(input("enter at least 6 character and below than 14 characters  for username >>> ").lower())
+        if len(username) < 5 or len(username) > 14:
+            print("The user must fill more than 5 characters and bellow than 14")
+            continue
+        break
+    return username
 password_list=[]
 count_pass='default'
-while not count_pass.isnumeric():
-    count_pass=input("Enter your Password count --> ")
+username=request_username()
+print(username)
+while True:
+    try:
+        count_pass=int(input("Enter your Password count >>> "))
+        break
+    except:
+        print("Please Enter number for Count >>> ")
+        continue
 for count in range(0,10):
     gen_password=[]
     for i in range(0,int(count_pass)):
@@ -27,4 +42,4 @@ for count in range(0,10):
     password_list.append(''.join(gen_password))
 print("\n you choise from below list \n")
 for pass_list_number in range(0,10):
-    print('| {}.{} | \n'.format(pass_list_number+1,password_list[pass_list_number]))
+    print('| {}) {} | \n'.format(pass_list_number+1,password_list[pass_list_number]))
