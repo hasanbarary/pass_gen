e7e563438e0f8a57279338f6c7355864f73084f5
hasanbarary <46402575+hasanbarary@users.noreply.github.com>
2019-01-21 06:55:33 +0300

    generate passwor with simple mode and complex mode

--- pass-gen.py
+++ pass-gen.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3.6
-from random import randint
-def upper():
-    return chr(randint(65,90))
-def lower():
-    return chr(randint(97,122))
-def other():
-    return chr(randint(33,42))
-def number():
-    return str(randint(0,9))
+from random import choices 
+from string import punctuation,ascii_letters,digits
+
 def request_username():
     while True:
         username=str(input("enter at least 6 character and below than 14 characters  for username >>> ").lower())
@@ -16,29 +10,48 @@ def request_username():
             continue
         break
     return username
-password_list=[]
-count_pass='default'
-username=request_username()
-while True:
-    try:
-        count_pass=int(input("Enter your Password count >>> "))
-        break
-    except:
-        print("Please Enter number for Count >>> ")
-        continue
-for count in range(0,10):
-    gen_password=[]
-    for i in range(0,int(count_pass)):
-        rand=randint(1,4)
-        if rand == 1:
-            gen_password.append(upper())
-        elif rand == 2:
-            gen_password.append(lower())
-        elif rand == 3:
-            gen_password.append(other())
+def gen_password():
+    answer=int(input("Enter your length for generate pass >>> "))
+    for i in range(10):
+        password="".join(choices(digits+punctuation+ ascii_letters,k=answer))
+        print("{}). {}".format(i+1,password))
+def sim_gen_password():
+    charlist={
+          'a':('A','a','@','/\\','/-\\','4'),'b':('b','B','8','|3','13','!3','(3','|-]','j3'),
+          'c':('c','C','(','<','[','{'),'d':('d','D','|)','T)','|>','17','cI'),
+          'e':('e','E','3','[-','|=-'),'f':('F','f','|=','|#'),
+          'g':('g','G','6','9','C-','gee','(_+'),'h':('h','H','#','|-|',']-[',':-:','|~|','1-1','(-)'),
+          'i':('i','L','1','[]','|','!','eye','3y3',']['), 'j':('J','j',',_|','_|','._|','._]'),
+          'k':('K','k','>|','|<','/<','1<','|c','|C'),'l':('L','l','|_','7','|'),
+          'm':('M','m','nn','JVl','Jv|','|\\/|','^^'),'n':('N','n','^','^','|V','|\\|','{\\}','^/'),
+          'o':('O','o','0','Q','()','<>','[]'),'p':('|*','|O','|o','9','|>','|7'),
+          'q':('Q','q','<|','&','0_'),'r':('R','r','12','|^','|2','I2'),
+          's':('S','s','$','z','2'),'t':('T','t','+','7','"|"','-|-'),
+          'u':('U','u','(_)','|_|','V','v',),'v':('V','v'),
+          'w':('w','W','1N','\\N','(n)'),'x':('X','x','ecks',')(','><','}{'),
+          'y':('Y','y','j','4','7'),'z':('Z','z','2','7_','s','%','-|_')
+          }
+    gen_pass_list=[]
+    password=[]
+    print("You Username is {} choice from below list ")
+    for i  in range(10):
+        for char in username:
+            password+=random.choice(charlist[char])
+        gen_pass_list.append(''.join(password))
+        password=[]
+        print("{}). {}".format(i+1,gen_pass_list[i]))
+def help():
+    while True:
+        answer=int(input("1.simple and wihtout remember format with optional lenght.\n2.strong and with remember format with system set lenght.\n>>>"))
+        if answer==1:
+            return 1
+        elif answer ==2:
+            return 2
         else:
-            gen_password.append(number())
-    password_list.append(''.join(gen_password))
-print("\n you choise from below list \n")
-for pass_list_number in range(0,10):
-    print('| {}) {} | \n'.format(pass_list_number+1,password_list[pass_list_number]))
+            continue  
+    
+username=request_username()
+if help() == 1:
+    sim_gen_password()
+else:
+    gen_password()
